06b18aca8453899e18891e3446acf1d351e7d7ac
Alex Yang <alexyang.personal@gmail.com>
2013-10-15 20:58:21 -0700

    add minimal controller tests

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,7 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '.container .col-md-3', 3
   end
 
 end
