b358c03003177332069abf7c3e495b188baeee21
Alex Yang <alexyang.personal@gmail.com>
2013-10-15 17:40:45 -0700

    edit restaurant mvc

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ActiveRecord::Base
+
+  validates :name, :description, :image_url, presence: true
+  validates :name, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with:     %r{\.(gif|jpg|png)\Z}i,
+    message:  'must be a URL for GIF, JPG or PNG image.'
+  }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.delete_all
+Restaurant.create!( restaurant_name:  "California Fish Grill",
+              description:      "Seafood",
+              image_url:        "cfg.jpg")
+Restaurant.create!( restaurant_name:  "Fukada",
+              description:      "Japanese",
+              image_url:        "fukada.jpg")
+Restaurant.create!( restaurant_name:  "Mick's Karma Bar",
+              description:      "Burgers",
+              image_url:        "micks.jpg")
+Restaurant.create!( restaurant_name:  "Kula Revolving Sushi Bar",
+              description:      "Japanese Sushi Bar",
+              image_url:        "kula.jpg")
+Restaurant.create!( restaurant_name:  "Le Diplomate Cafe",
+              description:      "Sandwiches",
+              image_url:        "diplomate.gif")
