d585c043eb005a8a7a8e5f0e5200240b42aaacb2
Alex Yang <alexyang.personal@gmail.com>
2013-10-15 17:57:43 -0700

    clean up appearance of restaurant listings

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,18 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Restaurant.delete_all
-Restaurant.create!( restaurant_name:  "California Fish Grill",
-              description:      "Seafood",
-              image_url:        "cfg.jpg")
-Restaurant.create!( restaurant_name:  "Fukada",
-              description:      "Japanese",
-              image_url:        "fukada.jpg")
-Restaurant.create!( restaurant_name:  "Mick's Karma Bar",
-              description:      "Burgers",
-              image_url:        "micks.jpg")
-Restaurant.create!( restaurant_name:  "Kula Revolving Sushi Bar",
-              description:      "Japanese Sushi Bar",
-              image_url:        "kula.jpg")
-Restaurant.create!( restaurant_name:  "Le Diplomate Cafe",
-              description:      "Sandwiches",
-              image_url:        "diplomate.gif")
+Restaurant.create!( name:         "California Fish Grill",
+                    description:  "Seafood",
+                    image_url:    "cfg.jpg")
+Restaurant.create!( name:         "Fukada",
+                    description:  "Japanese",
+                    image_url:    "fukada.jpg")
+Restaurant.create!( name:         "Mick's Karma Bar",
+                    description:  "Burgers",
+                    image_url:    "micks.jpg")
+Restaurant.create!( name:         "Kula Revolving Sushi Bar",
+                    description:  "Japanese Sushi Bar",
+                    image_url:    "kula.jpg")
+Restaurant.create!( name:         "Le Diplomate Cafe",
+                    description:  "Sandwiches",
+                    image_url:    "diplomate.gif")
