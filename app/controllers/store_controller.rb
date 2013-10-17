3846766a8917a1e0cc8cc3996b18c23bb60b4baa
Alex Yang <alexyang.personal@gmail.com>
2013-10-16 17:04:06 -0700

    add error handling for nonexistent restaurant views

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,21 +1,21 @@
 class StoreController < ApplicationController
+  before_filter :check_restaurant, only: [:show]
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find_by name: (params[:restaurant_name])
     @menus = @restaurant.menus
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_menu
-      @menu = @restaurant.menus.find(params[:id])
+    def check_restaurant
+      @restaurant = Restaurant.find_by name: (params[:restaurant_name])
+      if @restaurant.nil?
+        redirect_to root_path, notice: "Restaurant does not exist"
+      end
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def menu_params
-      params.require(:menu).permit(:name, :description, :price)
-    end
 end
