8c65a95b0e86abb6209a69f6c75e9da2d4b50bd8
Alex Yang <alexyang.personal@gmail.com>
2013-10-24 16:19:31 -0700

    add name to devise User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131024085053) do
+ActiveRecord::Schema.define(version: 20131024221107) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20131024085053) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
