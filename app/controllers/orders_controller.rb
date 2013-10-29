a73634106db9f6af98d005fc74c5ebde05775b12
Alex Yang <alexyang.personal@gmail.com>
2013-10-28 18:03:07 -0700

    clean up views for order index and show pages

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,7 +6,7 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-    @orders = Order.all
+    @orders = Order.order("updated_at DESC")
   end
 
   # GET /orders/1
