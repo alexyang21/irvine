976f7606a5fcaeab39cec5de6f0842175ad0c92f
Alex Yang <alexyang.personal@gmail.com>
2013-11-08 17:44:22 -0800

    add clickable menu items and pg configuration for development

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131106050600) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "carts", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -27,9 +30,9 @@ ActiveRecord::Schema.define(version: 20131106050600) do
     t.integer  "order_id"
   end
 
-  add_index "items", ["cart_id"], name: "index_items_on_cart_id"
-  add_index "items", ["menu_id"], name: "index_items_on_menu_id"
-  add_index "items", ["order_id"], name: "index_items_on_order_id"
+  add_index "items", ["cart_id"], name: "index_items_on_cart_id", using: :btree
+  add_index "items", ["menu_id"], name: "index_items_on_menu_id", using: :btree
+  add_index "items", ["order_id"], name: "index_items_on_order_id", using: :btree
 
   create_table "menus", force: true do |t|
     t.string   "name"
@@ -41,7 +44,7 @@ ActiveRecord::Schema.define(version: 20131106050600) do
     t.string   "category"
   end
 
-  add_index "menus", ["restaurant_id"], name: "index_menus_on_restaurant_id"
+  add_index "menus", ["restaurant_id"], name: "index_menus_on_restaurant_id", using: :btree
 
   create_table "orders", force: true do |t|
     t.string   "name"
@@ -55,7 +58,7 @@ ActiveRecord::Schema.define(version: 20131106050600) do
     t.integer  "user_id"
   end
 
-  add_index "orders", ["user_id"], name: "index_orders_on_user_id"
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
@@ -89,7 +92,7 @@ ActiveRecord::Schema.define(version: 20131106050600) do
     t.boolean  "admin",                  default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
