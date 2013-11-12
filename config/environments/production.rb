1e654f9f4a135603c1ca74903a28942d95d6a314
Alex Yang <alexyang.personal@gmail.com>
2013-11-11 21:58:42 -0800

    fiddle with production.rb for email again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ FlipOrder::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :port                 => '465',
+    :port                 => '587',
     :address              => "smtp.mandrillapp.com",
     :user_name            => ENV["MANDRILL_USERNAME"],
     :password             => ENV["MANDRILL_APIKEY"],
