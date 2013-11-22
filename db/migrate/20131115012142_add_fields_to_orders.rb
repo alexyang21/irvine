8e42835221bdfbaac3b019fe6fa26b053bb0631f
Alex Yang <alexyang.personal@gmail.com>
2013-11-21 17:22:06 -0800

    remove bad migration

--- db/migrate/20131115012142_add_fields_to_orders.rb
+++
@@ -1,7 +0,0 @@
-class AddFieldsToOrders < ActiveRecord::Migration
-  def change
-    add_column :orders, :date, :date
-    add_column :orders, :time, :time
-    add_column :orders, :driver_instructions, :text
-  end
-end
